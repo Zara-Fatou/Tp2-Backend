65ef2be03e4ce3cd140a2a49dcb6a3e0eda5bfda
RoseGodlove <2342394@cegeplimoilou.ca>
2025-11-03 12:56:12 -0500

    #11 Creation du repository des nouvelles

---
+++ TP2_Rose-Zara/TP2_Backend-Rose-Zara/src/main/java/a25/climoilou/web2/TP2_Rose_Zara/controller/NouvelleController.java
@@ -0,0 +1,9 @@
+package a25.climoilou.web2.TP2_Rose_Zara.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class NouvelleController {
+}
