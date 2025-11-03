2b84be5c9272c2b9fbffa82bb6ae2f5677f6e1ad
RoseGodlove <2342394@cegeplimoilou.ca>
2025-11-03 11:16:19 -0500

    le backend est cree

---
+++ TP2_Rose-Zara/TP2_Rose-Zara/src/main/java/a25/climoilou/web2/TP2_Rose_Zara/Tp2RoseZaraApplication.java
@@ -0,0 +1,13 @@
+package a25.climoilou.web2.TP2_Rose_Zara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2RoseZaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2RoseZaraApplication.class, args);
+	}
+
+}
