1bb66cd078302d929eed3bde1dcd5c3fc6761cee
RoseGodlove <2342394@cegeplimoilou.ca>
2025-11-06 09:22:13 -0500

    resolution des conflits 2

--- TP2_Rose-Zara/src/main/java/a25/climoilou/web2/TP2_Rose_Zara/repository/CitereRepository.java
+++
@@ -1,8 +0,0 @@
-package a25.climoilou.web2.TP2_Rose_Zara.repository;
-
-import a25.climoilou.web2.TP2_Rose_Zara.entity.Criteria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CitereRepository extends CrudRepository<Criteria,Long> {
-    Criteria findById(long id);
-}
